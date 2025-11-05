638bb1fd42883df17fe3db6ce014118622ae8840
EchoPrograms <brogan.oberhaus30@gmail.com>
2025-11-05 11:52:34 -0500

    added constants file

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Constants {
+namespace Renderer {
+
+constexpr char WINDOW_TITLE[] = "opengl";
+constexpr int WINDOW_WIDTH = 800;
+constexpr int WINDOW_HEIGHT = 600;
+
+} // namespace Renderer
+} // namespace Constants
